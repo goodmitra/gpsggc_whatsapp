f1897fdc148591c315044088ad3dc0a132f4aec1
goodmitra <120350653+goodmitra@users.noreply.github.com>
2023-01-08 19:31:46 +0000

    modified:   index.js 	new file:   view/index.ejs

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/* instruction
-node -v
-npm -v
-npm init 
-npm i whatsapp-web.js  (for whatsapp)
-npm i qrcode-terminal	(for whatsapp qr)
-create file index.js
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const qrcode1 = require("qrcode");
+const exp = require("constants");
 
-npm i --save express (for html)
-npm i --save fs (for html)
-npm i xlsx  (for excel)
+const app = express();
 
-
-*/
 
 const qrcode = require('qrcode-terminal');
 
@@ -24,8 +18,7 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
     console.log('Client is ready!');
-
-});
+	});
 
 client.initialize();
  
@@ -34,4 +27,50 @@ client.initialize();
 		client.sendMessage(message.from, 'pong');
 	}
 });
- 
+
+const port = process.env.port || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.get('/wa', async (req, res) => {
+ //   const client = new Client(...)
+    let qr = await new Promise((resolve, reject) => {
+        client.on('qr', (qr) => resolve(qr))
+    })
+	qrcode1.toDataURL(qr, (err, src) => {
+    if (err) res.send("Something went wrong!!");
+    res.render("scan", {
+      qr_code: src,
+    });
+	});
+    //res.send(qr)
+	/*
+	 qrcode1.toDataURL(qr, (err, src) => {
+    if (err) res.send("Something went wrong!!");
+    res.render("scan", {
+      qr_code: src,
+    });*/
+	 //res.send('<img src="'qr'">');
+});
+
+
+app.post("/scan", (req, res, next) => {
+  const input_text = req.body.text;
+  qrcode1.toDataURL(input_text, (err, src) => {
+    if (err) res.send("Something went wrong!!");
+    res.render("scan", {
+      qr_code: src,
+    });
+  });
+});
+app.listen(port, console.log(`Listening on port ${port}`));
