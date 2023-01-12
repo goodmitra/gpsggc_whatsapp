944af11ad0cef0aaf5141f888074461a77428307
goodmitra <120350653+goodmitra@users.noreply.github.com>
2023-01-12 10:23:43 +0000

    modified:   index.js 	new file:   ramji.js 	modified:   view/user1.ejs

--- ramji.js
+++ ramji.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 
 const qrcode = require('qrcode-terminal');
 
-const { Client, MessageMedia } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 /*
@@ -34,7 +34,6 @@ client.initialize();
 client.on('message', async (message) => {
 	if(message.body === 'gps') {
 		client.sendMessage(message.from, 'Hello Sir How Can I Help You');
-    console.log(message.from);
 	}
 	else if(message.body === 'hello') {
 		client.sendMessage(message.from, 'Hello Sir How are You');
@@ -44,78 +43,43 @@ client.on('message', async (message) => {
 let age ="" ;
 let name ="";
 let wkey ="";
-let wfile ="";
-let sess="";
 
-app.get("/user", async (req, res)=>{  
-  name = req.query.name
-//  wkey = req.query.key
-  wfile = req.query.file;
-  let imk="Yes";
-  let mimeType;  
-  const fileUrl=wfile;
+app.get("/user", function(req, res){  
+   name = req.query.name
+   wkey = req.query.key
+ // var age = req.query.age
+  age = JSON.parse(req.query.array);    
+  console.log("Name :", name)
+  console.log("Age :", age)
+  console.log("Key :", wkey)
+  let sess="";
+  //res.render("user1", { name: name, number: age, wkey: wkey });
+  if(clai === 1)
+  {
+    console.log('Ready'+clai);
+    sess="Ready";
+   // var number= '919001480042@c.us';
+   // var msjko= "hello hhh";
 
- age = JSON.parse(req.query.array);    
- console.log("Name :", name)
- console.log("Age :", age)
- console.log("file :", fileUrl)
- if(clai === 1)
- {
-   console.log('Ready'+clai);
-   sess="Ready"; 
-    mimeType = await MessageMedia.fromUrl('http://online.gpsggc.com/adminmata/'+wfile);
     for (const type of age) { 
-      let numbet= '91'+type+'@c.us';  
-      client.sendMessage(numbet, mimeType,{caption:name})
-      .then(response => {
-          console.log('Successfully! photo1');
-        //  res.status(200).json({  status: true,   response: response });
-      }).catch(err => {
-          console.log('Error! photo1'+err);
-        //  res.status(500).json({ status: false,response: err});
-      });
-    } 
- }
- else{
-   console.log('not Ready'+clai);
-   sess="Not ready";
- }
- res.render("user1", { name: name, number: age, wkey: imk, claty:sess });
-}) 
-
-
-app.get("/user2", async (req, res)=>{  
-  name = req.query.name
-//  wkey = req.query.key  
-  let imk="No"; 
-
- age = JSON.parse(req.query.array);    
- console.log("Name :", name)
- console.log("Age :", age) 
- if(clai === 1)
- {
-   console.log('Ready'+clai);
-   sess="Ready";    
-    for (const type of age) { 
-      let numbet= '91'+type+'@c.us';  
+      let numbet= '91'+type+'@c.us';
       client.sendMessage(numbet, name)
       .then(response => {
-          console.log('Successfully!');
+          console.log('Successfully! ');
         //  res.status(200).json({  status: true,   response: response });
       }).catch(err => {
-          console.log('Error! '+err);
+          console.log('Error! ');
         //  res.status(500).json({ status: false,response: err});
       });
-    } 
- }
- else{
-   console.log('not Ready'+clai);
-   sess="Not ready";
- }
- res.render("user1", { name: name, number: age, wkey: imk, claty:sess });
+    }
+  }
+  else{
+    console.log('not Ready'+clai);
+    sess="Not Ready";
+  }
+
+  res.render("user1", { name: name, number: age, wkey: wkey, claty: sess });
 }) 
-
-
 
 client.on('message', async (message, mass1, numb, wakey) => {
     mass1=name;
@@ -153,6 +117,41 @@ client.on('message', async (message, mass1, numb, wakey) => {
       }        
   
     }
+//key
+
+else if(wakey === '99'){
+ 
+    console.log("Name :", mass1)
+    console.log("Age :", numb)
+    console.log("Key :", wakey)
+      //const input_text = req.body.text;
+    //  var age = JSON.parse(req.query.array);
+    //const types = [ '9001480042','7014518593' ];
+    const types1 = numb;
+    const mass=mass1;
+    for (const type of types1) {  
+
+      const number = type;
+      const text = mass;
+      const sanitized_number = number.toString().replace(/[- )(]/g, ""); // remove unnecessary chars from the number
+      const final_number = `91${sanitized_number.substring(sanitized_number.length - 10)}`; // add 91 before the number here 91 is country code of India
+  
+      const number_details = await client.getNumberId(final_number); // get mobile number details
+  
+      if (number_details) {
+          const sendMessageData = await client.sendMessage(number_details._serialized, text); // send message
+      } else {
+          console.log(final_number, "Mobile number is not registered");
+      }
+      
+      
+  }  
+
+	}
+//key
+
+
+
 });
 
 const port = process.env.port || 5000;
@@ -167,6 +166,18 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res, next) => {
   res.render("index");
+
+  /*
+  let qr = await new Promise((resolve, reject) => {
+    client.on('qr', (qr) => resolve(qr))
+  })
+  qrcode1.toDataURL(qr, (err, src) => {
+  if (err) res.send("Something went wrong!!");
+  res.render("wa", {
+  qr_code: src,
+  });
+  });*/
+
 });
 
 app.post("/", async function(req, res) {
